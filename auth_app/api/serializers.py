48d4b175af7a9563ebc3962886558455e5b88026
Pinguinrakete <ekirschke@web.de>
2025-11-27 01:22:09 +0100

    add CookieTokenObtainPairSerializer to serializers.py in auth_app

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -38,4 +38,26 @@ class RegisterSerializer(serializers.ModelSerializer):
 
 
 class CookieTokenObtainPairSerializer(TokenObtainPairSerializer):
-    pass
+    email = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(
+        write_only=True, required=True, style={"input_type": "password"}
+    )
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        if not email or not password:
+            raise serializers.ValidationError(
+                "Both fields must be filled out."
+                )
+
+        email = authenticate(email=email, password=password)
+        if email is None:
+            raise serializers.ValidationError(
+                "Invalid email or password."
+                )
+
+        attrs["email"] = email
+
+        return attrs
