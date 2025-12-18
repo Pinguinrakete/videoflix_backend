17fcd0ba5eb26fe75cef72d874cd182ab0e2743f
Pinguinrakete <ekirschke@web.de>
2025-12-18 20:10:36 +0100

    Add delete function in content_app

--- content_app/api/signals.py
+++ content_app/api/signals.py
@@ -1,10 +1,23 @@
-from .models import Video
+import os
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.db.models.signals import post_delete
+from .models import Video
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
     print('Video wurde gespeichert')
     if created:
         print('New video created')
+
+
+@receiver(post_delete, sender=Video)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `Video` object is deleted.
+    """
+    if instance.file:
+        if os.path.isfile(instance.video_file.path):
+            os.remove(instance.video_file.path)
         
