8fcb0b5149285740fc9c34ee731e20ba97298ad9
Pinguinrakete <ekirschke@web.de>
2025-12-22 22:23:40 +0100

    Add VideoView in views.py in content_app

--- content_app/api/views.py
+++ content_app/api/views.py
@@ -1,10 +1,19 @@
-from rest_framework.views import APIView
 from auth_app.api.permissions import CookieJWTAuthentication
+from models import Video
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import VideoSerializer
 
 
 class VideoView(APIView):
     authentication_classes = [CookieJWTAuthentication]
-    pass
+
+    def get(self, request):
+
+        videos = Video.objects.all().order_by("-created_at")
+        serializer = VideoSerializer(videos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class HLSMasterPlaylistView(APIView):
